1ea54867d31edece14ef851f6f5cfab4baf51941
SonyaRyab <sonia.riabova@gmail.com>
2026-04-06 16:55:58 +0300

    add styles

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { ROUTES } from './Routes'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/sabatie-theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
